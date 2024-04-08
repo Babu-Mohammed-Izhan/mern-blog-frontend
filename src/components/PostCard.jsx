02a0a2a6c511c5b65693fa47ffddfe29c0bd68b5
git_mohammedraif13 <mohammedraif14@gmail.com>
2024-04-08 17:42:58 +0530

    remove images

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 export default function PostCard({ post }) {
   return (
     <div className='group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all'>
-      <Link to={`/post/${post.slug}`}>
+      {/* <Link to={`/post/${post.slug}`}>
         <img
           src={post.image}
           alt='post cover'
           className='h-[260px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20'
         />
-      </Link>
+      </Link> */}
       <div className='p-3 flex flex-col gap-2'>
         <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
         <span className='italic text-sm'>{post.category}</span>
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -70,11 +70,11 @@ export default function PostPage() {
           {post && post.category}
         </Button>
       </Link>
-      <img
+      {/* <img
         src={post && post.image}
         alt={post && post.title}
         className='mt-10 p-3 max-h-[600px] w-full object-cover'
-      />
+      /> */}
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
